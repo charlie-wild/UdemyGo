4e577ba5c9b5339071485392302f34e018e6a625
Charlie Wild <charlie.wild@thinkmoney.co.uk>
2020-01-17 15:59:15 +0000

    adding comments

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+// any time we make an executable package it must has a function called 'main'
+
+import "fmt" //form a link between main and fmt. same as JS require statement
 
 func main() {
 	fmt.Println("hello world")
