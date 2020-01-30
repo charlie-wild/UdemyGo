93088897bea491ac520b3903d2eff679fec4ecda
Charlie Wild <charlie.wild@thinkmoney.co.uk>
2020-01-30 08:55:08 +0000

    arrays and slices

--- cards/main.go
+++ cards/main.go
@@ -6,6 +6,11 @@ func main() {
 	// var card string = "Ace of Spades" - below is different syntax but equivalent
 	card := newCard()
 	fmt.Println(card)
+
+	// array is fixed length ; slice is C# list or JS array
+	// both must be defined with a data type
+
+	cards := []string{"Ace of Diamonds", newCard()}
 }
 
 // label the data type you expect to return from the function
