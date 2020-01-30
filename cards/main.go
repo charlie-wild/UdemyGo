8ec4b7d613be3f1b7580cfbfc281de02b5627392
charlie-wild <charliewild@gmail.com>
2020-01-30 09:10:45 +0000

    learnt slice syntax

--- cards/main.go
+++ cards/main.go
@@ -5,12 +5,17 @@ import "fmt"
 func main() {
 	// var card string = "Ace of Spades" - below is different syntax but equivalent
 	card := newCard()
-	fmt.Println(card)
 
 	// array is fixed length ; slice is C# list or JS array
 	// both must be defined with a data type
 
 	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades") //append returns a new slice
+
+	for i, card := range cards { //range takes each index of the whole slice
+		fmt.Println(i, card)
+	}
+
 }
 
 // label the data type you expect to return from the function
