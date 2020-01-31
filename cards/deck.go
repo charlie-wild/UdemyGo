89057550683d02505e6da0c30779e07e1bbca2c8
charlie-wild <charliewild@gmail.com>
2020-01-31 15:23:44 +0000

    set up receiver function

--- cards/deck.go
+++ cards/deck.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+/* Create a new type of deck which is a slice of strings */
+
+type deck []string
+
+func (d deck) print() { // any variable of type deck gets access to this print method using the receiver
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,20 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades" - below is different syntax but equivalent
-	card := newCard()
+	//card := newCard()
 
 	// array is fixed length ; slice is C# list or JS array
 	// both must be defined with a data type
 
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades") //append returns a new slice
 
-	for i, card := range cards { //range takes each index of the whole slice
-		fmt.Println(i, card)
-	}
+	cards.print()
 
 }
 
