2f2473a11411634b47efd965ac7660f60cebe71e
charlie-wild <charliewild@gmail.com>
2020-01-31 16:01:21 +0000

    iterarting over cards

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,19 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for i, suit := range cardSuits {
+		for j, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+}
+
 func (d deck) print() { // any variable of type deck gets access to this print method using the receiver
 	for i, card := range d {
 		fmt.Println(i, card)
