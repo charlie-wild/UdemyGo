068bae5b10316c7b381f3418191429fdfd1c5947
charlie-wild <charliewild@gmail.com>
2020-02-25 16:32:29 +0000

    pointer code added

--- structs/main.go
+++ structs/main.go
@@ -28,12 +28,14 @@ func main() {
 			zipCode: 90210, //remember the commas...
 		},
 	}
-	jim.updateName("Jimmy")
+	jimPointer := &jim
+
+	jimPointer.updateName("Jimmy")
 	jim.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
