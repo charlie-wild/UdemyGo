c1e7396e3de516cdad1c0895c1a64949625c3ad6
charlie-wild <charliewild@gmail.com>
2020-02-25 16:39:08 +0000

    pointer notes added

--- structs/main.go
+++ structs/main.go
@@ -28,14 +28,18 @@ func main() {
 			zipCode: 90210, //remember the commas...
 		},
 	}
-	jimPointer := &jim
+	jimPointer := &jim //the & gives us access to the RAM address where the variable is sitting
 
 	jimPointer.updateName("Jimmy")
 	jim.print()
 }
 
+// if we see a * where a type should be (as ln 39) it is a type description and not an operator as * is inside the function!
+
 func (pointerToPerson *person) updateName(newFirstName string) {
 	(*pointerToPerson).firstName = newFirstName
+
+	// the * says to give you the value at the memory address of the variable
 }
 
 func (p person) print() {
