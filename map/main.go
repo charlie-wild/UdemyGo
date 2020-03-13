6a4a693c162de121e394eaeeaffcd9352837b1fd
charlie-wild <charliewild@gmail.com>
2020-03-13 14:54:04 +0000

    maps syntax

--- map/main.go
+++ map/main.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	colors := map[string]string{
-		"red" : "#ff0000",
-		"green" : "#4bf745",
+		"red":   "#ff0000",
+		"green": "#4bf745",
 	}
 
+	colors["white"] = "#ffffff"
+
+	// no dot syntax available with maps
+
 	fmt.Println(colors)
+
 }
