ea2ac1fd35532c0a0f93de9699127b1876b07656
charlie-wild <charliewild@gmail.com>
2020-03-13 15:26:47 +0000

    loops in maps

--- map/main.go
+++ map/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+
+	// colors := make(map[int]string) alternative syntax
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
@@ -15,5 +18,12 @@ func main() {
 	delete(colors, "white")
 
 	fmt.Println(colors)
+	printMap(colors)
 
+}
+
+func printMap(c map[string]string) {
+	for color, key := range c {
+		fmt.Println(color, key)
+	}
 }
