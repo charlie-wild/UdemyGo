604c20ca28081df7c5b3e8e912a49a5a2718efe5
charlie-wild <charliewild@gmail.com>
2020-03-27 16:43:53 +0000

    basic interfaces

---
+++ Interfaces/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+// for receiver functions, we can eliminate the variable and only pass in the type, if we don't use it
+
+func (englishBot) getGreeting() string {
+	// super customer logic goes here
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
