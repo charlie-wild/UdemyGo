2025bf07a5d0814f8d55974d0609c40f4ca671d3
charlie-wild <charliewild@gmail.com>
2020-03-27 16:47:51 +0000

    more basic interfaces

--- Interfaces/main.go
+++ Interfaces/main.go
@@ -6,16 +6,21 @@ type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
 }
+
 
 func printGreeting(eb englishBot) {
 	fmt.Println(eb.getGreeting())
 }
 
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
-}
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
 
 // for receiver functions, we can eliminate the variable and only pass in the type, if we don't use it
 
