3423089c8035ea10248283352f9876bba6fcc09d
charlie-wild <charliewild@gmail.com>
2020-04-21 16:17:33 +0100

    syntax for get request

--- http/main.go
+++ http/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+
 func main()  {
-	
+	 resp, err := http.Get("http://google.co.uk")
+
+	 if err != nil {
+		 fmt.Println("Error:", err)
+		 os.Exit(1)
+	 }
 }
