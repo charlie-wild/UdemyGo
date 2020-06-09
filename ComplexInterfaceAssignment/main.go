68dde3433a4e5d6334d42dceb6ac60b41c36df09
Charlie Wild <charlie.wild@thinkmoney.co.uk>
2020-06-09 18:16:31 +0100

    interface assignment

--- ComplexInterfaceAssignment/main.go
+++ ComplexInterfaceAssignment/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Args) //print out command line arguments to program
+	file, err := os.Open(os.Args[1]) //os.Open takes an argument, returns a variable of type file and an error
+
+	if err != nil {
+		fmt.Println("Error is: ", err)
+		os.Exit(1) //exit the program if there is an error
+	}
+
+	io.Copy(os.Stdout, file) //can use io.Copy as the type File satisfies the reader interface
+
 }
